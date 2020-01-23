477b26f082c6806f541007d0af169d4df6937884
Aleksbgbg <kozichka01@abv.bg>
2020-01-23 20:19:26 +0000

    [Calculator] Add WinForms correctly-implemented calculator project.

---
+++ Calculator/Calculator/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    using System.Windows.Forms;
+
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
